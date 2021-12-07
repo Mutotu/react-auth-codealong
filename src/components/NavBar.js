68dfe0605ae04267619ffa3f1806ecac9629c3e2
joel-hughes-general-assembly <joel.hughes@generalassemb.ly>
2021-12-07 00:28:37 -0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,24 +3,11 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     return (
         <nav>
-            <h1>🔑  React Auth! 🔓</h1>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/signup">Sign up</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/profile">Profile</Link>
-                </li>
-                <li>
-                    <Link to="/logout">Logout</Link>
-                </li>
-            </ul>
+            <Link to="/">Home</Link>
+            <span >Logout</span>
+            <Link to="/signup">Sign up</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/profile">Profile</Link>
         </nav>
     )
 }
